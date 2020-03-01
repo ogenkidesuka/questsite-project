ccee92b50f18340aa5d8a896c439259ede9f794e
Your Name <you@example.com>
2020-03-01 12:11:19 +0300

    minor changes in admin tool

--- questsite_project/questapp/admin.py
+++ questsite_project/questapp/admin.py
@@ -4,5 +4,9 @@ from questapp.models import Game, Place
 # Register your models here.
 
 
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('game_id', 'code', 'clue', 'info')
+
+
 admin.site.register(Game)
-admin.site.register(Place)
+admin.site.register(Place, PlaceAdmin)
